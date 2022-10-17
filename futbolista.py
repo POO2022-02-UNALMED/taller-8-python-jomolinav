5d0ee908dda0c58b268cfd76382aadd78e3ffcaa
jomolinav <jomolinav@unal.edu.co>
2022-10-16 21:39:40 -0500

    10

--- futbolista.py
+++ futbolista.py
@@ -5,8 +5,7 @@ from deportista import Deportista
 class Futbolista(Persona, Deportista):
     listaFutbolistas = []
     def __init__(self, nombre, edad, altura, sexo, añosPracticando,  golesMarcados, tarjetasRojas, piernaHabil):
-        super().__init__(nombre, edad, altura, sexo)
-        super().__init__( añosPracticando)
+        super().__init__(nombre, edad, altura, sexo, añosPracticando)
         self.golesMarcados = golesMarcados
         self.tarjetasRojas = tarjetasRojas
         self.piernaHabil = piernaHabil
